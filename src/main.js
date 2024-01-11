3530f5c31971f8e04b49e7aead958bfac0ce8f57
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2024-01-11 20:54:21 +0530

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import '../styles/style.css';
+// import '../styles/selector.css';
+import '../styles/color.css';
+import dom from './util/dom';
+
+dom();
+
---
+++ src/util/dom.js
@@ -0,0 +1,7 @@
+const dom =()=>{
+    let head = document.getElementsByTagName('body');
+    console.log(head);
+    head.style.backgroundColor = "black";
+}
+
+export default dom;
