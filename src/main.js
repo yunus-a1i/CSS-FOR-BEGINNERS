1db81cb9e9849935606a18afc8e40fa33551843c
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2024-01-14 13:17:55 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import '../styles/style.css';
 // import '../styles/selector.css';
 // import '../styles/color.css';
-import '../styles/units.css';
+// import '../styles/units.css';
+import '../styles/box-model.css';
 import dom from './util/dom';
 
 dom();
