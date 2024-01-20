21c458c16cf573dcf43d336f0dd4f44bcb6ff40a
Yunus Ali Siddiqui <yunusalisiddiqui90@gmail.com>
2024-01-20 19:56:11 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,14 @@ import '../styles/style.css';
 // import '../styles/units.css';
 // import '../styles/box-model.css';
 // import '../styles/typography.css';
-import '../styles/css-links.css';
-
+// import '../styles/css-links.css';
+// import '../styles/list.css';
+// import '../styles/menu.css';
+// import '../styles/display.css';
+// import '../styles/floats.css';
+// import '../styles/column.css';
+// import '../styles/position.css';
+import '../styles/flexbox.css';
 
 // import dom from './util/dom';
 
